bb96a388b1f6aa9dcc609beb0165a880d6c08d65
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-02-15 14:59:33 +0000

    Further work on simulation and modelling

--- r/simulate_data.R
+++ r/simulate_data.R
@@ -6,6 +6,8 @@
 
 
 ## Load the saved data set
+library(dplyr)
+library(fabricatr)
 library(MASS)
 library(tidyverse)
 library(tidymodels)
@@ -13,90 +15,193 @@ library(ggdark)
 library(knitr)
 library(tidyr)
 
-## Two options we can either source the cleaning file...
-# source("r/master.R")
-## Or we can load the file that is saved at the end and use that
-df <- readRDS("data/r/clean.rds")
+#' simulate_data
+#'
+#' @description A function that simulates artificial data.
+#'
+#' @param prop_malignant float Proportion of individuals who have malignant tumors.
+#' @param gender_ratio float Gender ratio (default = 0.5).
+#' @param nodule_diameter_mean float Mean nodule maximum diameter in mm (default `24.82`)
+#' @param nodule_diameter_sd float Standard Deviation of nodule maximum diameter in mm (default `14.94`),
+#' @param age_mean float Mean age in years (default `54.37`)
+#' @param age_sd float Standard Deviation of age in years (default `16.65`),
+#' @param asa_score_boundary vector ASA score is generated from a scaled standard normal distribution (see `scaling`),
+#' these  are the boundaries for classifying as I/II/III/IV (default `c(-Inf, -2, 6, 12, Inf)`)
+#' @param smoking_boundary vector Smoking classification is generated from an unscaled normal distribution (i.e. N(0,
+#' 1)), #' these are the boundaries for classifying as Non-Smoker/Ex-Smoker/Current (default `c(-Inf, 0, 1, Inf)`).
+#' @param nodule_fna_thy_boundary vector FNA Thyroid Nodule classification is generated from a scaled standard normal
+#' distribution (see `scaling`), these are the boundaries for classifying as Thy1/Thy2/Thy3/Thy4/Thy5 (default `c(-Inf,
+#' -4, #' 1, 6, 11, Inf)`)
+#' @param scaling vector Factor by which random variables generated from a standard normal distribution (i.e. N(0, 1))
+#' are #' scaled (default 5). If you change this parameter you will likely have to change the `asa_score_boundary` and
+#' `nodule_fna_thy_boundary`.
+#' @param seed int Seed to ensure results are reproducible (default `17010880`).
+#'
+#' @return DataFrame of simulated data
+#'
+#' @noRd
+simulate_data <- function(N = 1000,
+                          prop_malignant = 0.1,
+                          gender_ratio = 0.5,
+                          nodule_diameter_mean = 24.82,
+                          nodule_diameter_sd = 14.94,
+                          age_mean = 54.37,
+                          age_sd = 16.65,
+                          asa_score_boundary = c(-Inf, -2, 6, 12, Inf),
+                          smoking_boundary = c(-Inf, 0, 1, Inf),
+                          nodule_fna_thy_boundary = c(-Inf, -4, 1, 6, 11, Inf),
+                          scaling = 5,
+                          seed = 17010880) {
+  ## Set a seed for random number generation. This ensures we get the same results each time the code is run.
+  set.seed(seed)
+  df <- fabricatr::fabricate(
+    N = N,
+    pathology = runif(N),
+    final_pathology = draw_binary(latent = pathology, link = "identity"),
+    sex = runif(N),
+    gender = draw_binary(prob = gender_ratio, N = N),
+    nodule_size = rnorm(N,
+      mean = nodule_diameter_mean,
+      sd = nodule_diameter_sd
+    ),
+    age = round(rnorm(N,
+      mean = age_mean,
+      sd = age_sd
+    )),
+    asa = scaling * rnorm(N),
+    asa_score = draw_ordered(
+      x = asa,
+      breaks = asa_score_boundary,
+      break_labels = c("I", "II", "III", "IV")
+    ),
+    smoker = rnorm(N),
+    smoking_status = draw_ordered(
+      x = smoker,
+      breaks = smoking_boundary,
+      break_labels = c("Non-Smoker", "Ex-smoker", "Current")
+    ),
+    nodule_fna = scaling * rnorm(N),
+    nodule_fna_thy = draw_ordered(
+      x = nodule_fna,
+      breaks = nodule_fna_thy_boundary,
+      break_labels = c("Thy1", "Thy2", "Thy3", "Thy4", "Thy5")
+    )
+  ) |>
+    dplyr::select(!c(pathology, sex, asa, smoker, nodule_fna)) |>
+    ## Convert variables to factors
+    dplyr::mutate(
+      final_pathology = case_match(
+        final_pathology,
+        0 ~ "Benign",
+        1 ~ "Malignant"
+      ),
+      final_pathology = as.factor(final_pathology),
+      gender = case_match(
+        gender,
+        0 ~ "Male",
+        1 ~ "Female"
+      ),
+      gender = as.factor(gender)
+    )
+
+  var_labels <- c(
+    ID = "ID",
+    final_pathology = "Malignancy status",
+    gender = "Gender",
+    nodule_size = "Nodule Size (mm)",
+    age = "Age (Years)",
+    asa_score = "ASA Score",
+    smoking_status = "Smoking Status",
+    nodule_fna_thy = "FNA Thy Status"
+  )
+  Hmisc::label(df) <- as.list(var_labels[match(names(df), names(var_labels))])
+  return(df)
+}
+
+
+##########################################################################
+## Old code written before discovering the fabricatr package            ##
+##########################################################################
 
 ## Set a seed for random number generation. This ensures we get the same results each time the code is run.
-set.seed(17010880)
+## set.seed(17010880)
 
 ## Create a varianc co-variance matrix
-sigma <- rbind(
-  c(1.1265924, 0.6387496, -0.2630270, 0.2875068),
-  c(0.6387496, 1.2053968, 0.8055271, -0.2540865),
-  c(0.2630270, 0.8055271, 1.1482047, -0.4735176),
-  c(0.2875068, -0.2540865, -0.4735176, 1.0012329)
-)
-det(sigma)
+## sigma <- rbind(
+##   c(1.1265924, 0.6387496, -0.2630270, 0.2875068),
+##   c(0.6387496, 1.2053968, 0.8055271, -0.2540865),
+##   c(0.2630270, 0.8055271, 1.1482047, -0.4735176),
+##   c(0.2875068, -0.2540865, -0.4735176, 1.0012329)
+## )
+## det(sigma)
 
 ## Generate the mean vector
-mu <- c(44, 10, 6, 3)
+## mu <- c(44, 10, 6, 3)
 
 ## Generate the multivariate normal distribution
-dummy <- as.data.frame(MASS::mvrnorm(n = nrow(df), mu = mu, Sigma = sigma))
+## dummy <- as.data.frame(MASS::mvrnorm(n = nrow(df), mu = mu, Sigma = sigma))
 
 ## Generate binary outcome variable with noise
-dummy <- dummy |>
-  mutate(
-    final_pathology_dummy = ifelse(V4 > median(V4),
-      sample(c(0, 1), n(), replace = TRUE, p = c(0.25, 0.75)),
-      sample(c(0, 1), n(), replace = TRUE, p = c(0.75, 0.25))
-    ),
-    ## final_pathology_dummy = case_when(
-    ##   V4 <= median(V4) ~ sample(c("Bengin", "Malignant"),
-    ##     n(),
-    ##     replace = TRUE,
-    ##     p = c(0.25, 0.75)),
-    ##   TRUE ~ sample(c("Bengin", "Malignant"),
-    ##     n(),
-    ##     replace = TRUE,
-    ##     p = c(0.75, 0.25))),
-    dummy_cat_1 = case_when(
-      V3 < quantile(V3, 0.25) ~ sample(c("a", "b", "c", "d"),
-        n(),
-        replace = TRUE,
-        p = c(0.7, 0.1, 0.1, 0.1)
-      ),
-      V3 < quantile(V3, 0.50) ~ sample(c("a", "b", "c", "d"),
-        n(),
-        replace = TRUE,
-        p = c(0.1, 0.7, 0.1, 0.1)
-      ),
-      V3 < quantile(V3, 0.75) ~ sample(c("a", "b", "c", "d"),
-        n(),
-        replace = TRUE,
-        p = c(0.1, 0.1, 0.7, 0.1)
-      ),
-      TRUE ~ sample(c("a", "b", "c", "d"),
-        n(),
-        replace = TRUE,
-        p = c(0.1, 0.1, 0.7, 0.1)
-      )
-    ),
-    dummy_cat_2 = case_when(
-      V2 < quantile(V2, 0.15) ~ sample(c("I", "II", "III", "IV"),
-        n(),
-        replace = TRUE,
-        p = c(0.8, 0.1, 0.05, 0.05)
-      ),
-      V2 < quantile(V2, 0.65) ~ sample(c("I", "II", "III", "IV"),
-        n(),
-        replace = TRUE,
-        p = c(0.1, 0.8, 0.05, 0.05)
-      ),
-      V2 < quantile(V2, 0.85) ~ sample(c("I", "II", "III", "IV"),
-        n(),
-        replace = TRUE,
-        p = c(0.1, 0.05, 0.8, 0.05)
-      ),
-      TRUE ~ sample(c("I", "II", "III", "IV"),
-        n(),
-        replace = TRUE,
-        p = c(0.1, 0.05, 0.05, 0.8)
-      )
-    )
-  )
-GGally::ggpairs(dummy)
+## dummy <- dummy |>
+##   mutate(
+##     final_pathology_dummy = ifelse(V4 > median(V4),
+##       sample(c(0, 1), n(), replace = TRUE, p = c(0.25, 0.75)),
+##       sample(c(0, 1), n(), replace = TRUE, p = c(0.75, 0.25))
+##     ),
+##     final_pathology_dummy = case_when(
+##       V4 <= median(V4) ~ sample(c("Bengin", "Malignant"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.25, 0.75)),
+##       TRUE ~ sample(c("Bengin", "Malignant"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.75, 0.25))),
+##     dummy_cat_1 = case_when(
+##       V3 < quantile(V3, 0.25) ~ sample(c("a", "b", "c", "d"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.7, 0.1, 0.1, 0.1)
+##       ),
+##       V3 < quantile(V3, 0.50) ~ sample(c("a", "b", "c", "d"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.1, 0.7, 0.1, 0.1)
+##       ),
+##       V3 < quantile(V3, 0.75) ~ sample(c("a", "b", "c", "d"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.1, 0.1, 0.7, 0.1)
+##       ),
+##       TRUE ~ sample(c("a", "b", "c", "d"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.1, 0.1, 0.7, 0.1)
+##       )
+##     ),
+##     dummy_cat_2 = case_when(
+##       V2 < quantile(V2, 0.15) ~ sample(c("I", "II", "III", "IV"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.8, 0.1, 0.05, 0.05)
+##       ),
+##       V2 < quantile(V2, 0.65) ~ sample(c("I", "II", "III", "IV"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.1, 0.8, 0.05, 0.05)
+##       ),
+##       V2 < quantile(V2, 0.85) ~ sample(c("I", "II", "III", "IV"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.1, 0.05, 0.8, 0.05)
+##       ),
+##       TRUE ~ sample(c("I", "II", "III", "IV"),
+##         n(),
+##         replace = TRUE,
+##         p = c(0.1, 0.05, 0.05, 0.8)
+##       )
+##     )
+##   )
+## GGally::ggpairs(dummy)
 
-df_dummy <- cbind(df, dummy)
+## df_dummy <- cbind(df, dummy)
--- r/tidymodel.R
+++ r/tidymodel.R
@@ -37,43 +37,252 @@ df <- readRDS("data/r/clean.rds")
 ##
 ## This is done in the file r/simulate_data.R and this will NOT be needed in the final work.
 source("r/simulate_data.R")
-
-
-
+dummy <- simulate_data()
+summary(dummy)
 
 ## Split Data
 ##
 ## We now split the data into test and training data using an explicit seed value to ensure we get the same results each
 ## time.
-set.seed(5039378)
-split <- initial_split(df, prop = 0.75)
-train <- training(split)
-test <- testing(split)
-
-## Cross Validation
+## Split Data
 ##
-## We create cross validation folds on the training data set
-folds <- vfold_cv(train, v = 10, repeats = 10)
+## We now split the data into test and training data using an explicit seed value to ensure we get the same results each
+## time.
+#| label: test-train-split
+#| eval: true
+#| echo: true
+#| output: false
+set.seed(5039378)
+split <- rsample::initial_split(dummy, prop = 0.75)
+train <- rsample::training(split)
+test <- rsample::testing(split)
 
 ## Build a Recipe
 ##
 ## In Tidymodels a recipe is a series of pre-processing steps that are taken
-dummy_recipe <- df_dummy |>
-  recipes::recipe(data = train) |>
-  recipes::step_dummy() |>
-  recipes::step_normalize(all_numeric())
+#| label: recipe
+#| eval: true
+#| echo: true
+#| output: false
+thyroid_recipe <- recipes::recipe(final_pathology ~ gender + nodule_size + age + asa_score + smoking_status + nodule_fna_thy, data = train) |>
+  # recipes::step_num2factor(final_pathology, levels = c("Benign", "Malignant")) |>
+  recipes::step_dummy(gender, asa_score, smoking_status, nodule_fna_thy) |>
+    recipes::step_normalize(all_numeric())
+
+
+## Cross Validation
+##
+## We create cross validation folds on the training data set. There is vfold (often referred to as k-fold) and leave one
+## out (loo) cross-validation created here.
+#| label: cv-vfold
+#| eval: true
+#| echo: true
+#| output: false
+cv_folds <- rsample::vfold_cv(train, v = 10, repeats = 10)
+
+#| label: cv-loo
+#| eval: true
+#| echo: true
+#| output: false
+cv_loo <- rsample::loo_cv(train)
+
 
 ## Create a Workflow
 ##
 ## Create a workflow consists of a recipe and different models. The recipe is run before models are and so we add the
 ## recipe to dummy_workflow and will later add models to it.
-dummy_workflow <- workflows::workflow() |>
-  workflows::add_recipe(dummy_recipe)
+#| label: workflow
+#| eval: true
+#| echo: true
+#| output: false
+thyroid_workflow <- workflows::workflow() |>
+    workflows::add_recipe(thyroid_recipe)
 
 
 ## LASSO, Ridge regression and Elastic Net
 ##
 ## We wish to perform logistic regression but it is well known that step-wise variable selection is highly problematic
 ## (they often result in over-fitting to the training data and are highly prone to the order in which variables are
-## added/removed).  A better alternative is to use Least Absolute Shrinkage and Selection Operator (LASSO)
-tune_lasso <- parsnip::logistic_reg(penalty = tune(), mixture)
+## added/removed).  A better alternative is to use Least Absolute Shrinkage and Selection Operator (LASSO) or Ridge
+## Regression. These are opposite ends of the spectrum in terms of two forms of penalties/regularisation termed L1 and
+## L2. L1 is useful for feature selection as it allows/encourages coefficients for predictor variables in the model to
+## tend towards zero, i.e. they have negligible effect. L2 regularisation helps when there are predictor variables that
+## are collinear/co-dependant (i.e. correlated variables) which increases the variability of the coefficients.
+##
+## We fit the model using the parsnip::logistic_reg() function with a mixture value of 0.5 which gives an equal trade
+## off to LASSO (L1) and Ridge Regression (L2) regularisation. This is termed an "Elastic Net", a value of 0 for mixture
+##is full ridge-regression whilst a value of 1 is a full LASSO model (i.e. all L1 and no L2).
+## The model is fitted using the "engine" glmnet.
+#| label: lasso-specification
+#| eval: true
+#| echo: true
+#| output: false
+tune_spec_lasso <- parsnip::logistic_reg(penalty = hardhat::tune(), mixture = 0.5) |>
+    parsnip::set_engine("glmnet")
+
+## Cross validate the model; using tune::tune_grid(), this takes the worflow (thyroid_workflow) that has been defined
+## along with the model we wish to fit (in terms of model type and parameters) and the cross validation we wish to
+## perform and then fits the model repeatedly by sampling from the training data set.
+#| label: lasso-kfold-tune
+#| eval: true
+#| echo: true
+#| output: false
+lasso_grid <- tune::tune_grid(
+  object = workflows::add_model(thyroid_workflow, tune_spec_lasso),
+  resamples = cv_folds,
+  grid = dials::grid_regular(penalty(), levels = 50)
+)
+
+## We now extract the model with the best Receiver Operating Characterisitc Area Under the Curve...
+#| label: lasso-kfold-best
+#| eval: true
+#| echo: true
+#| output: false
+lasso_kfold_roc_auc <- lasso_grid |>
+    tune::select_best(metric = "roc_auc")
+
+## ...and fit that model one last time to get our final LASSO model.
+#| label: lasso-kfold-fit-final
+#| eval: true
+#| echo: true
+#| output: false
+final_lasso_kfold <- tune::finalize_workflow(
+  workflows::add_model(thyroid_workflow, tune_spec_lasso),
+  lasso_kfold_roc_auc
+)
+
+
+## We take that last fir and extract the metrics from it. Here the accuracy and ROC AUC are roughly 50% which means the
+## ability to categorise people into "Malignant" or "Benign" based on the variables we chose is no better than chance
+## alone. This is NOT surprising as we are currently working with simulated data!
+#| label: lasso-kfold-eval-metrics
+#| eval: true
+#| echo: true
+#| output: false
+tune::last_fit(object = final_lasso_kfold, split = split) |>
+    tune::collect_metrics()
+
+## We can get an indication of the importance of variables in the model to see what the coefficients for each are.
+#| label: lasso-kfold-eval-importance
+#| eval: true
+#| echo: true
+#| output: false
+final_lasso_kfold |>
+  fit(train) |>
+  hardhat::extract_fit_parsnip() |>
+  vip::vi(lambda = lasso_kfold_roc_auc$penalty) |>
+  dplyr::mutate(
+    Importance = abs(Importance),
+    Variable = fct_reorder(Variable, Importance)
+  ) |>
+  ggplot(mapping = aes(x = Importance, y = Variable, fill = Sign)) +
+  geom_col() +
+  dark_theme_minimal()
+
+## The Tidymodel framework allows us to use the same workflow that we setup to easily run other types of modelling. Here
+## we use a Random Forest to classify. Random Forests itteratively search through the variables to work out which
+## provides the best method of classifying people into either "Malignant" or "Benign". It tries all variables in
+## turn. For binary variables such as gender this is straight-forward. For continuous variables a number of different
+## thresholds for dichotomising the variable are tested for each possible split and the optimal cut-point is selected.
+## This process is repeated so that after for example after selecting gender to be a strong predictor the remaining
+## variables will tested for their predictive ability and the best selected. The number of trees that are created and
+## tested is defined by the 'trees' parameter which here is set to 100.
+#| label: random-forest-specify
+#| eval: true
+#| echo: true
+#| output: false
+rf_tune <- parsnip::rand_forest(
+    mtry = tune(),
+    trees = 100,
+    min_n = tune()) |>
+  set_mode("classification") |>
+    set_engine("ranger", importance = "impurity")
+
+## As before the process is repeated using Cross validation.
+#| label: random-forest-tune
+#| eval: true
+#| echo: true
+#| output: false
+rf_grid <- tune::tune_grid(
+    add_model(thyroid_workflow, rf_tune),
+    resamples = cv_folds, ## cv_loo,
+    grid = grid_regular(mtry(range = c(5, 10)), # smaller ranges will run quicker
+        min_n(range = c(2, 25)),
+        levels = 3
+    )
+)
+## ...and the best model based on the ROC AUC selected...
+#| label: random-forest-model
+#| eval: true
+#| echo: true
+#| output: false
+rf_highest_roc_auc <- rf_grid |>
+    select_best("roc_auc")
+final_rf <- tune::finalize_workflow(
+    add_model(thyroid_workflow, rf_tune),
+    rf_highest_roc_auc
+)
+
+## ...and the metrics calculated.
+#| label: random-forest-evaluate
+#| eval: true
+#| echo: true
+#| output: false
+tune::last_fit(final_rf, split) |>
+    tune::collect_metrics()
+
+#| label: rf-kfold-eval-importance
+#| eval: true
+#| echo: true
+#| output: false
+final_rf|>
+  fit(train) |>
+  hardhat::extract_fit_parsnip() |>
+  vip::vi(lambda = final_rf$penalty) |>
+  dplyr::mutate(
+    Importance = abs(Importance),
+    Variable = fct_reorder(Variable, Importance)
+  ) |>
+  ggplot(mapping = aes(x = Importance, y = Variable, fill = Sign)) +
+  geom_col() +
+  dark_theme_minimal()
+
+
+#| label: svm-specify
+#| eval: true
+#| echo: true
+#| output: false
+svm_tune_spec <- svm_rbf(cost = tune()) |>
+    set_engine("kernlab") |>
+    set_mode("classification")
+
+#| label: svm-tune-hyperparameters
+#| eval: true
+#| echo: true
+#| output: false
+svm_grid <-  tune::tune_grid(
+  workflows::add_model(thyroid_workflow, svm_tune_spec),
+  resamples = cv_folds, ## cv_loo,
+  grid = dials::grid_regular(cost(), levels = 20)
+)
+
+#| label: svm-model
+#| eval: true
+#| echo: true
+#| output: false
+svm_highest_roc_auc <- svm_grid |>
+  tune::select_best("roc_auc")
+final_scm <- tune::finalize_workflow(
+  add_model(thyroid_workflow, svm_tune_spec),
+  svm_highest_roc_auc
+)
+
+#| label: svm-evaluate
+#| eval: true
+#| echo: true
+#| output: false
+tune::last_fit(final_rf, split,
+               metrics = metric_set(roc_auuc, accuracy, f_meas)) |>
+    tune::collect_metrics()
+
+## Create a confusion matrix
